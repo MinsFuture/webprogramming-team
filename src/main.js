c1c25f75fc51c0a4be54fb46b456d38a7e47cf66
이민혁 <helloaway213@gmail.com>
2024-05-30 22:25:11 +0900

    init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from "@/store";
+import axios from "axios";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+// Axios 전역 설정
+app.config.globalProperties.$axios = axios;
+
+app.use(store);
+app.use(router);
+
+app.mount('#app');
---
+++ src/router.js
@@ -0,0 +1,16 @@
+import { createWebHistory, createRouter } from "vue-router";
+import App from "@/App.vue";
+
+const routes = [
+    {
+        path: "/",
+        component: App
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
